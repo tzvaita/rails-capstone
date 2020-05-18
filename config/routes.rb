ff2fc7d18b5defe6c58a69d4de1ecf4da2242ae7
tzvaita <tzvaita@gmail.com>
2020-05-18 10:43:52 +0200

    Update users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'user/index'
-  get 'user/new'
-  get 'user/show'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
