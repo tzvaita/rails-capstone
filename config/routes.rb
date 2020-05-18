3f709b06190b7fc0de833cb60400e01bba750a91
tzvaita <tzvaita@gmail.com>
2020-05-18 11:21:16 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/show'
   get 'users/new'
+
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
