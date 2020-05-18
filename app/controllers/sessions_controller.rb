783eb981aa125ffc63c43d08462e4d11c90f5758
tzvaita <tzvaita@gmail.com>
2020-05-18 12:10:49 +0200

    Create a login method with sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid username'
+      render 'new'
+    end
+  end
+
+  def destroy
+
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,6 @@
 module SessionsHelper
+  # creates a session for the current user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 end
