dc37e11be8fe52e11379c41001ed20ae98867187
tzvaita <tzvaita@gmail.com>
2020-05-18 10:57:28 +0200

    Add user manipulation methods'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
+  # show all users
   def index
   end
 
+  # show a single user
   def show
   end
 
+  # new user instance
   def new
+    @user = User.new
   end
+
+  # create a new user
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+  
+  # allowed params
+    def user_params
+      params.require(user).permit(:fullname, :username)
+    end
 end
