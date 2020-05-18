20d7c88822ea5adf1fffd717f86e10f051fe7e81
tzvaita <tzvaita@gmail.com>
2020-05-18 11:40:41 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
+  get     '/login',    to: 'sessions#new'
+  post    '/login',    to: 'sessions#create'
+  delete  '/logout',   to: 'sessions#destroy'
+  get     'users/index'
+  get     'users/show'
+  get     'users/new'
 
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
