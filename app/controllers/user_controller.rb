d9b9938f7c42b0a5015cd4ab5874d89e63e84564
tzvaita <tzvaita@gmail.com>
2020-05-18 10:41:01 +0200

    Add Users controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,10 @@
+class UserController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
---
+++ test/controllers/user_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class UserControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get user_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get user_new_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get user_show_url
+    assert_response :success
+  end
+
+end
