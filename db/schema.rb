88cfd5b76db117059a8019058bb4d45f2a3f4aa0
tzvaita <tzvaita@gmail.com>
2020-05-19 12:08:30 +0200

    Add photo column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_191439) do
+ActiveRecord::Schema.define(version: 2020_05_19_100649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_05_18_191439) do
     t.string "fullname"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo"
   end
 
 end
