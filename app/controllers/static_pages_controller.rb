529bba48804c7a5811204e55e11a2a16078b9968
tzvaita <tzvaita@gmail.com>
2020-05-27 22:08:44 +0200

    Work on n+1 queries

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
 
   def home
     @thought = current_user.thoughts.build if logged_in?
-    @thoughts = Thought.all.order(created_at: :desc)
+    @thoughts = Thought.order(created_at: :desc).includes(:author)
     @users = User.all.order(created_at: :desc)
   end
 end
