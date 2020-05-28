6b970457dddf63a49e5e945745639bce73dd9784
tzvaita <tzvaita@gmail.com>
2020-05-28 18:41:30 +0200

    Add more integration tests

---
+++ spec/features/thoughts_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "Thoughts", type: :feature do
+  before :each do
+    visit signup_path
+    expect(page.current_path).to eq '/signup'
+    page.fill_in 'Fullname', with: 'foobar'
+    page.fill_in 'Username', with: 'bar'
+    click_button 'Create my account'
+    expect(page).to have_text 'Welcome to the App!'
+    expect(page.current_path).to eq '/home'
+  end
+
+  scenario 'create a thought' do
+    within('form') do
+    fill_in 'Text', with: 'First thought'
+    end
+    click_button 'Post'
+    expect(page).to have_text 'Thought created!'
+  end
+end
